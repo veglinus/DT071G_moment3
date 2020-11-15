a39578f92bb32a0fccc3c383be361bd80108f5da
Linus <linushvenfelt@gmail.com>
2020-11-15 20:59:20 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace moment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
