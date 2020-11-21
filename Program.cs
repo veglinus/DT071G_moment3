96da50b17f8b8eb480c09344800c5dc2915cac7b
Linus <linushvenfelt@gmail.com>
2020-11-21 23:18:43 +0100

    automatic formatting

--- Program.cs
+++ Program.cs
@@ -8,73 +8,87 @@ namespace moment3
     public class Post
     {
         //public Guid ID {get; set;} // Uncomment for GUID functionability
-        public string Author {get; set;}
-        public string Data {get; set;}
-        public DateTime Time {get; set;}
+        public string Author { get; set; }
+        public string Data { get; set; }
+        public DateTime Time { get; set; }
     }
 
     public class Guestbook
     {
-    public void showPosts() {
-        try
+        public void showPosts()
         {
-            var JSONdata = ReadFile();
-            
-            if (JSONdata.Count != 0) { // If list is not empy
-                int index = 0;
-                foreach (var item in JSONdata) // For each post in list
-                {
-                    Console.WriteLine($"[{index}] {item.Author} - {item.Data}"); //  display index, author and data
-                    index++;
-                }
-            } else {
-                Console.WriteLine("No entries in guestbook.");
-            }
-        }
-        catch (System.Exception)
-        {
-            Console.WriteLine("Error in guestbook initialization."); // catch errors
-        }
-    }
-
-    public void add()
-    {
-    var person = new Post();
-    enterName();
-        void enterName(string err = "") {
             try
             {
-                if (err != "") {
-                    Console.WriteLine($"Error: {err}");
-                }
-                Console.WriteLine("What is your name?");
-                string author = Console.ReadLine(); // await input
+                var JSONdata = ReadFile();
 
-                if (author.Length < 3) {
-                    enterName("Name too short. Minimum is 3 characters.");
-                } else {
-                    person.Author = author; // set author
-                    enterMessage(); // continue to message
+                if (JSONdata.Count != 0)
+                { // If list is not empy
+                    int index = 0;
+                    foreach (var item in JSONdata) // For each post in list
+                    {
+                        Console.WriteLine($"[{index}] {item.Author} - {item.Data}"); //  display index, author and data
+                        index++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No entries in guestbook.");
                 }
             }
             catch (System.Exception)
             {
-                enterName("Unknown error.");
+                Console.WriteLine("Error in guestbook initialization."); // catch errors
             }
         }
 
-        void enterMessage(string err = "") {
-            try
+        public void add()
+        {
+            var person = new Post();
+            enterName();
+            void enterName(string err = "")
+            {
+                try
                 {
-                    if (err != "") {
+                    if (err != "")
+                    {
+                        Console.WriteLine($"Error: {err}");
+                    }
+                    Console.WriteLine("What is your name?");
+                    string author = Console.ReadLine(); // await input
+
+                    if (author.Length < 3)
+                    {
+                        enterName("Name too short. Minimum is 3 characters.");
+                    }
+                    else
+                    {
+                        person.Author = author; // set author
+                        enterMessage(); // continue to message
+                    }
+                }
+                catch (System.Exception)
+                {
+                    enterName("Unknown error.");
+                }
+            }
+
+            void enterMessage(string err = "")
+            {
+                try
+                {
+                    if (err != "")
+                    {
                         Console.WriteLine($"Error: {err}");
                     }
                     Console.WriteLine($"{person.Author}, what would you like to post?");
                     string data = Console.ReadLine(); // await input
 
-                    if (data.Length < 3) {
+                    if (data.Length < 3)
+                    {
                         enterMessage("Post too short. Minimum is 3 characters.");
-                    } else {
+                    }
+                    else
+                    {
                         person.Data = data; // set input
                         person.Time = DateTime.Now; // also store timestamp
                         //person.ID = Guid.NewGuid(); // Uncomment for GUID functionability
@@ -89,88 +103,90 @@ namespace moment3
                 {
                     enterMessage("Unknown error.");
                 }
-        }
-    }
-    
-
-    public void delete(bool err = false)
-    {
-        Console.Clear();
-        Console.WriteLine("LINUS' GUESTBOOK");
-        if (err) {
-            Console.WriteLine("Error in deletion, please try again.");
-        }
-        Console.WriteLine("Which post would you like to delete? (Or type X to go back)");
-        showPosts();
-
-        try
-        {
-            var line = Console.ReadLine();
-            if (line != "X") {
-                int number = int.Parse(line); // at what index are we removing
-                var list = ReadFile();
-                list.RemoveAt(number); // Remove object in list at index = number written by user
-                SaveFile(list); // save list and refresh
             }
         }
-        catch (System.Exception)
+
+
+        public void delete(bool err = false)
         {
-            delete(true);
+            Console.Clear();
+            Console.WriteLine("LINUS' GUESTBOOK");
+            if (err)
+            {
+                Console.WriteLine("Error in deletion, please try again.");
+            }
+            Console.WriteLine("Which post would you like to delete? (Or type X to go back)");
+            showPosts();
+
+            try
+            {
+                var line = Console.ReadLine();
+                if (line != "X")
+                {
+                    int number = int.Parse(line); // at what index are we removing
+                    var list = ReadFile();
+                    list.RemoveAt(number); // Remove object in list at index = number written by user
+                    SaveFile(list); // save list and refresh
+                }
+            }
+            catch (System.Exception)
+            {
+                delete(true);
+            }
+
+            refresh();
         }
 
-        refresh();
-    }
-
-    public List<Post> ReadFile()
-    {
-        try
+        public List<Post> ReadFile()
         {
-            List<Post> newlist = new List<Post> {}; 
-            var loadfile = File.ReadAllText("data.json"); // Read file
-            newlist = JsonSerializer.Deserialize<List<Post>>(loadfile); // Deserialize file into our empty list
-            return newlist;
+            try
+            {
+                List<Post> newlist = new List<Post> { };
+                var loadfile = File.ReadAllText("data.json"); // Read file
+                newlist = JsonSerializer.Deserialize<List<Post>>(loadfile); // Deserialize file into our empty list
+                return newlist;
+            }
+            catch (System.Exception)
+            {
+                return new List<Post> { }; // if error, return empty list
+            }
+
         }
-        catch (System.Exception)
+
+        public void SaveFile(List<Post> newlist)
         {
-            return new List<Post> {}; // if error, return empty list
+            var newData = JsonSerializer.Serialize(newlist); // Serialize our list into JSON
+            File.WriteAllText("data.json", newData); // Write the new list
         }
-
-    }
-
-    public void SaveFile(List<Post> newlist)
-    {
-        var newData = JsonSerializer.Serialize(newlist); // Serialize our list into JSON
-        File.WriteAllText("data.json", newData); // Write the new list
-    }
 
         public void refresh()
         {
-        Console.Clear();
-        Console.WriteLine("LINUS' GUESTBOOK");
-        Console.WriteLine("1. Add new post");
-        Console.WriteLine("2. Delete post");
-        Console.WriteLine("X. Exit (Or any other key)\n");
-        showPosts();
+            Console.Clear();
+            Console.WriteLine("LINUS' GUESTBOOK");
+            Console.WriteLine("1. Add new post");
+            Console.WriteLine("2. Delete post");
+            Console.WriteLine("X. Exit (Or any other key)\n");
+            showPosts();
 
-        // Wait for input
-        var keypressed = Console.ReadKey(true).Key;
-        switch (keypressed)
-        { // switch case for what key is pressed by user
-            case ConsoleKey.X:
-                // nothing, exits program
-                break;
-            case ConsoleKey.D1:
-                add();
-                break;
-            case ConsoleKey.D2:
-                delete();
-                break;
+            // Wait for input
+            var keypressed = Console.ReadKey(true).Key;
+            switch (keypressed)
+            { // switch case for what key is pressed by user
+                case ConsoleKey.X:
+                    // nothing, exits program
+                    break;
+                case ConsoleKey.D1:
+                    add();
+                    break;
+                case ConsoleKey.D2:
+                    delete();
+                    break;
 
-            default: // Any other key simply exits program
-            // exits program
-            break;
-        }
-        
+                default: // Any other key simply exits program
+                         // exits program
+                    break;
+            }
+
         }
     }
 
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# DT071G - Moment 3 - Gästbok
+# DT071G - Moment 3 - Guestbook
+
+Commandline guestbook made in C#. Stores the data in a JSON file.
 
 Run with: `dotnet run`
-
-Todo:
-
-* Filtrering vid inmatning
-* testa med tom fil, ta bort allt, etc
